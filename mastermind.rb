ed794bad536fa5c44928f78a51339ed8ee66ed36
Qwib-Qwib <jp93rom@orange.fr>
2022-10-10 20:56:19 +0200

    Divide a big part of the computer AI into two modules called MoveProcessing and Minmax, tasked with editing available guesses remaining and computing most destructive guesses respectively. Put the blocks from the various move deleting and counting methods into lambdas, since they share the same between each version. Adapt the existing code so that it calls to the right modules without Rubocop pissing itself. The only Rubocop entry remaining is the one about the reduce, but I don't see any reason to change it, it works well enough for what I want.

--- mastermind.rb
+++ mastermind.rb
@@ -295,8 +295,218 @@ class Player
   end
 end
 
+# Tasked with processing feedback from a CPU's moves in order to clean up the CPU's set of possible guesses.
+module MoveProcessing
+  @zero_match_block = ->(potential_solution, compared_move) { potential_solution.intersect?(compared_move) == true }
+  @color_presence_block = lambda do |potential_solution, compared_move, partial_matches, perfect_matches|
+    enough_colors_to_match_feedback?(compared_move, potential_solution, partial_matches, perfect_matches) == false
+  end
+  @incorrect_position_block = lambda do |potential_solution, compared_move|
+    same_placement?(compared_move, potential_solution) == true
+  end
+  @perfect_position_block = lambda do |potential_solution, compared_move, perfect_matches|
+    exact_color_matching?(compared_move, potential_solution, perfect_matches) == false
+  end
+
+  class << self
+    attr_accessor :zero_match_block, :color_presence_block, :incorrect_position_block, :perfect_position_block
+  end
+
+  module_function
+
+  def move_deletion(decision_set, compared_move, partial_matches, perfect_matches)
+    zero_match_delete(decision_set, compared_move, &@zero_match_block) if partial_matches.zero? && perfect_matches.zero?
+    # The following is a guard clause meant to end method prematurely if condition not met.
+    return unless (partial_matches + perfect_matches).positive?
+
+    positive_feedback_move_deletion(decision_set, compared_move, partial_matches, perfect_matches)
+  end
+
+  def positive_feedback_move_deletion(decision_set, compared_move, partial_matches, perfect_matches)
+    if partial_matches.positive? && perfect_matches.zero?
+      incorrect_position_delete(decision_set, compared_move, &@incorrect_position_block)
+    end
+    color_presence_delete(decision_set, compared_move, partial_matches, perfect_matches, &@color_presence_block)
+    return unless perfect_matches.positive?
+
+    perfect_position_delete(decision_set, compared_move, perfect_matches, &@perfect_position_block)
+  end
+
+  def zero_match_delete(decision_set_used, compared_move, &block)
+    decision_set_used.delete_if { |potential_solution| block.call(potential_solution, compared_move) }
+  end
+
+  def color_presence_delete(decision_set_used, compared_move, partial_matches, perfect_matches, &block)
+    decision_set_used.delete_if do |potential_solution|
+      block.call(potential_solution, compared_move, partial_matches, perfect_matches)
+    end
+  end
+
+  def enough_colors_to_match_feedback?(compared_move, potential_solution, partial_matches, perfect_matches)
+    editable_solution = potential_solution.dup
+    matches = 0
+    compared_move.each do |color|
+      if editable_solution.include?(color)
+        editable_solution.delete_at(editable_solution.index(color))
+        matches += 1
+      end
+    end
+    partial_matches + perfect_matches == matches
+  end
+
+  def incorrect_position_delete(decision_set_used, compared_move, &block)
+    decision_set_used.delete_if { |potential_solution| block.call(potential_solution, compared_move) }
+  end
+
+  def perfect_position_delete(decision_set_used, compared_move, perfect_matches, &block)
+    decision_set_used.delete_if { |potential_solution| block.call(potential_solution, compared_move, perfect_matches) }
+  end
+
+  def same_placement?(compared_move, potential_solution)
+    compared_move.each_index do |index|
+      return true if compared_move[index] == potential_solution[index]
+    end
+    false
+  end
+
+  def exact_color_matching?(compared_move, potential_solution, perfect_matches)
+    matches = 0
+    compared_move.each_index do |index|
+      matches += 1 if compared_move[index] == potential_solution[index]
+    end
+    matches == perfect_matches
+  end
+
+  def zero_match_count(decision_set_used, compared_move, &block)
+    decision_set_used.count { |potential_solution| block.call(potential_solution, compared_move) }
+  end
+
+  def incorrect_position_count(decision_set_used, compared_move, &block)
+    decision_set_used.count { |potential_solution| block.call(potential_solution, compared_move) }
+  end
+
+  def color_presence_count(decision_set_used, compared_move, partial_matches, perfect_matches, &block)
+    decision_set_used.count do |potential_solution|
+      block.call(potential_solution, compared_move, partial_matches, perfect_matches)
+    end
+  end
+
+  def perfect_position_count(decision_set_used, compared_move, perfect_matches, &block)
+    decision_set_used.count { |potential_solution| block.call(potential_solution, compared_move, perfect_matches) }
+  end
+end
+
+# Tasked with performing Knuth's 5-moves-max algorithm by calculating which unused guess is more likely to trim down
+# the set of possible solutions.
+module Minmax
+  include MoveProcessing
+
+  module_function
+
+  def check_most_destructive_guess
+    @unused_guesses_set.delete(@last_move)
+    scores_per_guess = create_scores_list
+    scores_per_guess = scores_per_guess.sort_by { |_unused_guess, min_score| min_score }.reverse!
+    scores_per_guess.select! { |_unused_guess, min_score| min_score == scores_per_guess.first[1] }
+    scores_per_guess.flatten!(1).keep_if { |element| element.instance_of?(Array) }
+    if scores_per_guess.intersect?(@possible_solutions)
+      scores_per_guess.intersection(@possible_solutions).sample
+    else
+      scores_per_guess.sample
+    end
+  end
+
+  def create_scores_list
+    @unused_guesses_set.reduce({}) do |scores_list, unused_guess|
+      scores_list[unused_guess] = calculate_scores_for_one_guess(unused_guess)
+      scores_list[unused_guess].sort!
+      scores_list[unused_guess] = scores_list[unused_guess][0]
+      scores_list
+    end
+  end
+
+  def calculate_scores_for_one_guess(unused_guess)
+    possible_feedbacks = calculate_possible_feedbacks
+    scores_for_one_guess = []
+    possible_feedbacks.each_with_object(unused_guess) do |specific_feedback, guess|
+      dup_guesses_set = @possible_solutions.dup
+      scores_for_one_guess.push(minmax_check(specific_feedback, guess, dup_guesses_set))
+    end
+    scores_for_one_guess
+  end
+
+  def calculate_possible_feedbacks
+    possible_feedbacks_array = []
+    (0..4).to_a.repeated_permutation(2) do |possible_feedback|
+      possible_feedbacks_array.push(possible_feedback) if possible_feedback.sum <= 4 && possible_feedback[1] != 4
+    end
+    possible_feedbacks_array
+  end
+
+  def minmax_check(feedback, unused_guess, dup_guesses_set)
+    feedback_scores = []
+    compute_destruction_scores_for_unused_guess(feedback_scores, feedback, unused_guess, dup_guesses_set)
+    feedback_scores.sum
+  end
+
+  def compute_destruction_scores_for_unused_guess(feedback_scores, feedback, guess, guesses_set)
+    if feedback == [0, 0]
+      feedback_scores, guesses_set = get_zero_match_score(feedback_scores, guesses_set, guess,
+                                                          &MoveProcessing.zero_match_block)
+    end
+    if feedback.sum.positive?
+      feedback_scores = compute_destruction_scores_positive_feedback(feedback_scores, feedback, guess, guesses_set)
+    end
+    feedback_scores
+  end
+
+  def compute_destruction_scores_positive_feedback(feedback_scores, feedback, guess, guesses_set)
+    feedback_scores, guesses_set = get_color_presence_score(feedback_scores, guesses_set, guess, *feedback,
+                                                            &MoveProcessing.color_presence_block)
+    compute_destruction_scores_either_positive(feedback_scores, feedback, guess, guesses_set)
+  end
+
+  def compute_destruction_scores_either_positive(feedback_scores, feedback, guess, guesses_set)
+    if feedback[0].positive? && feedback[1].zero?
+      feedback_scores, guesses_set = get_incorrect_position_score(feedback_scores, guesses_set, guess,
+                                                                  &MoveProcessing.incorrect_position_block)
+    end
+    if feedback[1].positive?
+      feedback_scores = get_perfect_position_score(feedback_scores, guesses_set, guess, feedback[1],
+                                                   &MoveProcessing.perfect_position_block)
+    end
+    feedback_scores
+  end
+
+  def get_zero_match_score(feedback_scores, dup_guesses_set, unused_guess, &block)
+    feedback_scores.push(zero_match_count(dup_guesses_set, unused_guess, &block))
+    zero_match_delete(dup_guesses_set, unused_guess, &block)
+    [feedback_scores, dup_guesses_set]
+  end
+
+  def get_incorrect_position_score(feedback_scores, dup_guesses_set, unused_guess, &block)
+    feedback_scores.push(incorrect_position_count(dup_guesses_set, unused_guess, &block))
+    incorrect_position_delete(dup_guesses_set, unused_guess, &block)
+    [feedback_scores, dup_guesses_set]
+  end
+
+  def get_color_presence_score(feedback_scores, dup_guesses_set, unused_guess, *feedback, &block)
+    feedback_scores.push(color_presence_count(dup_guesses_set, unused_guess, *feedback, &block))
+    color_presence_delete(dup_guesses_set, unused_guess, *feedback, &block)
+    [feedback_scores, dup_guesses_set]
+  end
+
+  def get_perfect_position_score(feedback_scores, dup_guesses_set, unused_guess, perfect_matches, &block)
+    feedback_scores.push(perfect_position_count(dup_guesses_set, unused_guess, perfect_matches, &block))
+    perfect_position_delete(dup_guesses_set, unused_guess, perfect_matches, &block)
+    feedback_scores
+  end
+end
+
 # The ComputerPlayer class saves data about potential computer players and define their AI.
 class ComputerPlayer < Player
+  include Minmax
+  include MoveProcessing
   @computers_number = 1
 
   class << self
@@ -365,157 +575,7 @@ class ComputerPlayer < Player
     @possible_solutions.delete(@last_move)
     partial_matches = board.previous_suggestions.last['feedback'].count('o')
     perfect_matches = board.previous_suggestions.last['feedback'].count("\e[31mo\e[0m")
-    move_deletion(@possible_solutions, @last_move, partial_matches, perfect_matches)
-  end
-
-  def move_deletion(decision_set_used, compared_move, partial_matches, perfect_matches)
-    zero_match_delete(decision_set_used, compared_move) if partial_matches.zero? && perfect_matches.zero?
-    incorrect_position_delete(decision_set_used, compared_move) if partial_matches.positive? && perfect_matches.zero?
-    if (partial_matches + perfect_matches).positive?
-      color_presence_delete(decision_set_used, compared_move, partial_matches, perfect_matches)
-    end
-    perfect_position_delete(decision_set_used, compared_move, perfect_matches) if perfect_matches.zero? == false
-  end
-
-  def zero_match_delete(decision_set_used, compared_move)
-    decision_set_used.delete_if { |potential_solution| potential_solution.intersect?(compared_move) == true }
-  end
-
-  def color_presence_delete(decision_set_used, compared_move, partial_matches, perfect_matches)
-    decision_set_used.delete_if do |potential_solution|
-      enough_colors_to_match_feedback?(compared_move, potential_solution, partial_matches, perfect_matches) == false
-    end
-  end
-
-  def enough_colors_to_match_feedback?(compared_move, potential_solution, partial_matches, perfect_matches)
-    editable_solution = potential_solution.dup
-    matches = 0
-    compared_move.each do |color|
-      if editable_solution.include?(color)
-        editable_solution.delete_at(editable_solution.index(color))
-        matches += 1
-      end
-    end
-    partial_matches + perfect_matches == matches
-  end
-
-  def incorrect_position_delete(decision_set_used, compared_move)
-    decision_set_used.delete_if { |potential_solution| same_placement?(compared_move, potential_solution) == true }
-  end
-
-  def perfect_position_delete(decision_set_used, compared_move, perfect_matches)
-    decision_set_used.delete_if { |potential_solution| exact_color_matching?(compared_move, potential_solution, perfect_matches) == false }
-  end
-
-  def same_placement?(compared_move, potential_solution)
-    compared_move.each_index do |index|
-      return true if compared_move[index] == potential_solution[index]
-    end
-    false
-  end
-
-  def exact_color_matching?(compared_move, potential_solution, perfect_matches)
-    matches = 0
-    compared_move.each_index do |index|
-      matches += 1 if compared_move[index] == potential_solution[index]
-    end
-    matches == perfect_matches
-  end
-
-  def check_most_destructive_guess
-    @unused_guesses_set.delete(@last_move)
-    scores_per_guess = create_scores_list
-    scores_per_guess = scores_per_guess.sort_by { |_unused_guess, min_score| min_score }.reverse!
-    scores_per_guess.select! { |_unused_guess, min_score| min_score == scores_per_guess.first[1] }
-    scores_per_guess.flatten!(1).keep_if { |element| element.instance_of?(Array) }
-    if scores_per_guess.intersect?(@possible_solutions)
-      scores_per_guess.intersection(@possible_solutions).sample
-    else
-      scores_per_guess.sample
-    end
-  end
-
-  def create_scores_list
-    @unused_guesses_set.reduce({}) do |scores_list, unused_guess|
-      scores_list[unused_guess] = calculate_scores_for_one_guess(unused_guess)
-      scores_list[unused_guess].sort!
-      scores_list[unused_guess] = scores_list[unused_guess][0]
-      scores_list
-    end
-  end
-
-  def calculate_scores_for_one_guess(unused_guess)
-    possible_feedbacks = calculate_possible_feedbacks
-    scores_for_one_guess = []
-    possible_feedbacks.each_with_object(unused_guess) do |specific_feedback, guess|
-      dup_guesses_set = @possible_solutions.dup
-      scores_for_one_guess.push(minmax_check(specific_feedback, guess, dup_guesses_set))
-    end
-    scores_for_one_guess
-  end
-
-  def calculate_possible_feedbacks
-    possible_feedbacks_array = []
-    (0..4).to_a.repeated_permutation(2) do |possible_feedback|
-      possible_feedbacks_array.push(possible_feedback) if possible_feedback.sum <= 4 && possible_feedback[1] != 4
-    end
-    possible_feedbacks_array
-  end
-
-  def minmax_check(feedback, unused_guess, dup_guesses_set)
-    feedback_score = []
-    compute_destruction_scores(feedback_score, feedback, unused_guess, dup_guesses_set)
-    feedback_score.sum
-  end
-
-  def compute_destruction_scores(feedback_score, feedback, unused_guess, dup_guesses_set)
-    feedback_score, dup_guesses_set = get_zero_match_score(feedback_score, dup_guesses_set, unused_guess) if feedback == [0, 0]
-    feedback_score, dup_guesses_set = get_incorrect_position_score(feedback_score, dup_guesses_set, unused_guess) if feedback[0].positive? && feedback[1].zero?
-    feedback_score, dup_guesses_set = get_color_presence_score(feedback_score, dup_guesses_set, unused_guess, *feedback) if feedback.sum.positive?
-    feedback_score = get_perfect_position_score(feedback_score, dup_guesses_set, unused_guess, feedback[1]) if feedback[1].positive?
-    feedback_score
-  end
-
-  def get_zero_match_score(feedback_score, dup_guesses_set, unused_guess)
-    feedback_score.push(zero_match_count(dup_guesses_set, unused_guess))
-    zero_match_delete(dup_guesses_set, unused_guess)
-    [feedback_score, dup_guesses_set]
-  end
-
-  def get_incorrect_position_score(feedback_score, dup_guesses_set, unused_guess)
-    feedback_score.push(incorrect_position_count(dup_guesses_set, unused_guess))
-    incorrect_position_delete(dup_guesses_set, unused_guess)
-    [feedback_score, dup_guesses_set]
-  end
-
-  def get_color_presence_score(feedback_score, dup_guesses_set, unused_guess, *feedback)
-    feedback_score.push(color_presence_count(dup_guesses_set, unused_guess, *feedback))
-    color_presence_delete(dup_guesses_set, unused_guess, *feedback)
-    [feedback_score, dup_guesses_set]
-  end
-
-  def get_perfect_position_score(feedback_score, dup_guesses_set, unused_guess, perfect_matches)
-    feedback_score.push(perfect_position_count(dup_guesses_set, unused_guess, perfect_matches))
-    perfect_position_delete(dup_guesses_set, unused_guess, perfect_matches)
-    feedback_score
-  end
-
-  def zero_match_count(decision_set_used, compared_move)
-    decision_set_used.count { |potential_solution| potential_solution.intersect?(compared_move) == true }
-  end
-
-  def incorrect_position_count(decision_set_used, compared_move)
-    decision_set_used.count { |potential_solution| same_placement?(compared_move, potential_solution) == true }
-  end
-
-  def color_presence_count(decision_set_used, compared_move, partial_matches, perfect_matches)
-    decision_set_used.count do |potential_solution|
-      enough_colors_to_match_feedback?(compared_move, potential_solution, partial_matches, perfect_matches) == false
-    end
-  end
-
-  def perfect_position_count(decision_set_used, compared_move, perfect_matches)
-    decision_set_used.count { |potential_solution| exact_color_matching?(compared_move, potential_solution, perfect_matches) == false }
+    MoveProcessing.move_deletion(@possible_solutions, @last_move, partial_matches, perfect_matches)
   end
 end
 
